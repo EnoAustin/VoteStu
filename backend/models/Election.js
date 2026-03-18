7868bceb4d0fc46aff8af8e66102f787ef791256
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-03-18 12:20:06 +0100

    fix: correct syntax and formatting in Election and Vote models

--- backend/models/Election.js
+++ backend/models/Election.js
@@ -1,32 +1,36 @@
 // Everything about the election
 
-const mongoose = require ('mongoose')
+const mongoose = require("mongoose");
 
-const electionSchema = new mongoose.Schema({
-    title:{
-        type: String,
-        required: true
+const electionSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
     description: {
+      type: String,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+    posts: [
+      {
         type: String,
-    },
-    startDate{
-        type: Date,
-        required: true
-    },
-    endDate{
-        type: Date,
-        required: true
-    },
-    isActive{
-        type: Boolean,
-        default: false
-    },
-    posts:[{
-        type: String
-    }]
-}, {timestamps: true}
-)
-const Election = mongoose.model('Election', electionSchema)
+      },
+    ],
+  },
+  { timestamps: true },
+);
+const Election = mongoose.model("Election", electionSchema);
 
-module.exports = Exports
+module.exports = Election;
