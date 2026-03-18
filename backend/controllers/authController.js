8fc098ac34f3e17e484460eb1635c9135bf3ff0b
Eno Austin <austineno@icloud.com>
2026-03-18 23:27:43 +0100

    Save my work

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,112 +1,109 @@
-const Student = require('../models/Student')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-
+const Student = require("../models/Student");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const registerStudent = async (req, res) => {
-    try{
-        const{firstName, lastName,matricule, email} = req.body
+  try {
+    const { firstName, lastName, matricule, email } = req.body;
 
-        // Here we're checking students existence
-        const existingStudent = await Student.findOne ({matricule})
+    // Here we're checking students existence
+    const existingStudent = await Student.findOne({ matricule });
 
-        if(existingStudent){
-            return res.status(400).json({message: 'This student already exists'})
-        }
-
-        // If the student doesn't exist we create a student
-        const student = new Student({
-            firstName,
-            lastName,
-            matricule,
-            email,
-            password:'notset',
-            profilePhoto: req.file ? req.file.path: '',
-            isActivated: false
-        })
-
-        await student.save()
-        res.status(201).json({
-            message: 'Student registered successfully',
-            student
-        })
-    } catch(error){
-        res.status(500).json({message: 'Server error', error: error.message})
+    if (existingStudent) {
+      return res.status(400).json({ message: "This student already exists" });
     }
-}
+
+    // If the student doesn't exist we create a student
+    const student = new Student({
+      firstName,
+      lastName,
+      matricule,
+      email,
+      password: "notset",
+      profilePhoto: req.file ? req.file.path : "",
+      isActivated: false,
+    });
+
+    await student.save();
+    res.status(201).json({
+      message: "Student registered successfully",
+      student,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
 const activateAccount = async (req, res) => {
-    try{
-        const {matricule, password, confirmPassword } = req.body
+  try {
+    const { matricule, password, confirmPassword } = req.body;
 
-        // Let's verify if the passwords user wrote are thesame
-        if(password !== confirmPassword){
-            return res.status(400).json({message: 'Passwords do not match'})
-        }
-        // Here we're checking to see if student exists in the database
-        const student = await Student.findOne({matricule })
-        if(!student){
-            return res.status(404).json({message: "Student doesn't exist"})
-        }
-        if(student.isActivated){
-            return res.status(400).json({message:'Account already Activated'})
-        }
-        // Let's hash the password
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password, salt)
-
-        // Let's update student
-        student.password = hashedPassword
-        student.isActivated = true
-        await student.save()
-
-
-        res.status(200).json({message: 'Account Activated successfully'})
+    // Let's verify if the passwords user wrote are thesame
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: "Passwords do not match" });
     }
-    catch(error){
-        res.status(500).json({message: 'Server error', error: error.message})
+    // Here we're checking to see if student exists in the database
+    const student = await Student.findOne({ matricule });
+    if (!student) {
+      return res.status(404).json({ message: "Student doesn't exist" });
     }
-}
+    if (student.isActivated) {
+      return res.status(400).json({ message: "Account already Activated" });
+    }
+    // Let's hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Let's update student
+    student.password = hashedPassword;
+    student.isActivated = true;
+    await student.save();
+
+    res.status(200).json({ message: "Account Activated successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
 const loginStudent = async (req, res) => {
-    try{
-        const {matricule, password} = req.body
+  try {
+    const { matricule, password } = req.body;
 
-        // We'll find a student
-        const student = await Student.findOne({matricule})
-        if(!student) {
-            return res.status(404).json({message: 'Student not foung'})
-        }
-
-        // Verifying if account has been activated
-        if(!student.isActivated){
-            return res.status(400).json({message: 'Please activate your account'})
-        }
-
-        // Comparing password entered by student
-        const isMatch = await bcrypt.compare(password, student.password)
-        if(!isMatch){
-            return res.status(400).json({message:'Invalid credentials'})
-        }
-
-        // Creating Json Web Token
-        const token = jwt.sign(
-            { id: student._id, matricule: student.matricule},
-            process.env.JWT_SECRET,
-            {expiresIn:'8h'}
-        )
-        res.status(200).json({
-            message:'Login successful',
-            token,
-            student: {
-                id: student._id,
-                firstName: student.firstName,
-                lastName: student.lastName,
-                matricule: student.matricule,
-                hasVoted: student.hasVoted
-            }
-        })
-    }catch(error){
-        res.status(500).json({message:'Server error', error: error.message})
+    // We'll find a student
+    const student = await Student.findOne({ matricule });
+    if (!student) {
+      return res.status(404).json({ message: "Student not foung" });
     }
-}
 
-module.exports = {registerStudent, activateAccount, loginStudent}
+    // Verifying if account has been activated
+    if (!student.isActivated) {
+      return res.status(400).json({ message: "Please activate your account" });
+    }
+
+    // Comparing password entered by student
+    const isMatch = await bcrypt.compare(password, student.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // Creating Json Web Token
+    const token = jwt.sign(
+      { id: student._id, matricule: student.matricule },
+      process.env.JWT_SECRET,
+      { expiresIn: "8h" },
+    );
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      student: {
+        id: student._id,
+        firstName: student.firstName,
+        lastName: student.lastName,
+        matricule: student.matricule,
+        hasVoted: student.hasVoted,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+module.exports = { registerStudent, activateAccount, loginStudent };
--- backend/models/Candidate.js
+++ backend/models/Candidate.js
@@ -1,57 +1,58 @@
-// Candidate section 
+// Candidate section
 
-const mongoose = require ('mongoose')
+const mongoose = require("mongoose");
 
-const candidateSchema = new mongoose.Schema({
+const candidateSchema = new mongoose.Schema(
+  {
     firstName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     lastName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    matricule:{
-        type: String,
-        required: true
+    matricule: {
+      type: String,
+      required: true,
     },
-    election{
-        type: mongoose.Schema.Type.ObjectId,
-        ref: 'Election',
-        required: true
+    election: {
+      type: mongoose.Schema.Type.ObjectId,
+      ref: "Election",
+      required: true,
     },
     post: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     bio: {
-        type: String,
-        default: ''
+      type: String,
+      default: "",
     },
     feesDocument: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
-    isApproved:{
-        type: Boolean,
-        default: false
+    isApproved: {
+      type: Boolean,
+      default: false,
     },
     isConfirmed: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     applicationDeadline: {
-        type: Date
+      type: Date,
     },
-    status:{
-        type: String,
-        enum:['pending', 'approved', 'rejected'],
-        default: 'pending'
-    }
-}, 
-{timestamps: true}
-)
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending",
+    },
+  },
+  { timestamps: true },
+);
 
-const Candidate = mongoose.model('Candidate', candidateSchema)
+const Candidate = mongoose.model("Candidate", candidateSchema);
 
-module.exports = Candidate
+module.exports = Candidate;
